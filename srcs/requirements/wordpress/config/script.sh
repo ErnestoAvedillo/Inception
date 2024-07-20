fc8d5c56a0cd3655e10ac956ba8dd205a7a0b876
ernesto.avedillo <eavedillo@yahoo.es>
2024-07-20 16:02:41 +0200

    ?

--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-
+echo WP_PATH=$WP_PATH
+echo MARIADB_DB_NAME=$MARIADB_DB_NAME
+echo MARIADB_USER=$MARIADB_USER
+echo MARIADB_USER_PASSWORD=$MARIADB_USER_PASSWORD
+echo MARIADB_HOST=$MARIADB_HOST
+echo DOMAIN_NAME=$DOMAIN_NAME
+echo WP_TITLE=$WP_TITLE
+echo WP_ADMIN_USER=$WP_ADMIN_USER
+echo WP_ADMIN_PASSWORD=$WP_ADMIN_PASSWORD
+echo WP_ADMIN_EMAIL=$WP_ADMIN_EMAIL
+echo WP_USER=$WP_USER
+echo WP_USER_EMAIL=$WP_USER_EMAIL
+echo WP_USER_ROLE=$WP_USER_ROLE
+echo WP_USER_PASSWORD=$WP_USER_PASSWORD
+echo ****************************************************
 if [ -d ${WP_PATH}"wp-admin" ]; then
 	 echo "WordPress core is already downloaded"
 else
