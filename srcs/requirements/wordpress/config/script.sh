c98a9e7b44faf93a4d1711d69efb2677f609128e
ernesto.avedillo <eavedillo@yahoo.es>
2024-07-21 15:59:52 +0200

    ?

--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -23,5 +23,7 @@ else
 	wp core install   --path="${WP_PATH}" --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}"  --admin_email="${WP_ADMIN_EMAIL}" --skip-email --allow-root
 	wp user create    --path="${WP_PATH}" "${WP_USER}" "${WP_USER_EMAIL}" --role="$WP_USER_ROLE" --user_pass="${WP_USER_PASSWORD}" --allow-root
 	wp theme activate twentytwentytwo --path="${WP_PATH}" --allow-root
+	chmod -R 777 ${WP_PATH} && chown -R www-data:www-data ${WP_PATH}
 fi
+
 /usr/sbin/php-fpm7.4 -F
