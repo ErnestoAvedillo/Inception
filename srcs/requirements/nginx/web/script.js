4a9bb177670b890d81a1c17ee6769072bc9f9c24
ernesto.avedillo <eavedillo@yahoo.es>
2024-07-27 15:09:47 +0200

    w

--- srcs/requirements/nginx/web/script.js
+++ srcs/requirements/nginx/web/script.js
@@ -2,3 +2,13 @@ document.getElementById('clickMeButton').addEventListener('click', function() {
     alert('Button clicked!');
 });
 
+
+document.getElementById('clickMeButton1').addEventListener('click', function() {
+    /srv/www/wordpress/index.php
+
+});
+
+document.getElementById('clickMeButton2').addEventListener('click', function() {
+    /srv/www/wordpress/wp-admin/about.php
+
+});
