ac912854b5ed77cc7c775e62dc322846194ca3f0
ernesto.avedillo <eavedillo@yahoo.es>
2024-07-27 14:21:44 +0200

    a

--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -19,9 +19,9 @@ if [ -d ${WP_PATH}"/wp-admin" ]; then
 else
 	echo "WordPress Installing"
 	wp core download  --path="${WP_PATH}" --allow-root
-	wp config create  --path="${WP_PATH}" --dbname="${MARIADB_NAME}" --dbuser="${MARIADB_USER}" --dbpass="${MARIADB_USER_PASSWORD}" --dbhost="${MARIADB_HOST}" --allow-root
 	wp core install   --path="${WP_PATH}" --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}"  --admin_email="${WP_ADMIN_EMAIL}" --skip-email --allow-root
 	wp user create    --path="${WP_PATH}" "${WP_USER}" "${WP_USER_EMAIL}" --role="$WP_USER_ROLE" --user_pass="${WP_USER_PASSWORD}" --allow-root
+	wp config create  --path="${WP_PATH}" --dbname="${MARIADB_NAME}" --dbuser="${MARIADB_USER}" --dbpass="${MARIADB_USER_PASSWORD}" --dbhost="${MARIADB_HOST}" --allow-root
 	wp theme activate twentytwentytwo --path="${WP_PATH}" --allow-root
 	chmod -R 777 ${WP_PATH} && chown -R www-data:www-data ${WP_PATH}
 fi
